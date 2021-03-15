7c1c820bcce017f7f03bd8bcef72bc90c6f593f7
Melinda <melistajohn@gmail.com>
2021-03-15 13:22:38 -0700

    File Structure setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './App.css';
-
+//importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+     <header>
+      <h1>Melinda's Todo List</h1>
+    </header>
+    <Form/>
+    <TodoList/>
     </div>
   );
 }
